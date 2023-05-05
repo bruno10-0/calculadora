23edb8da04f9b40d0071ad019889f9ddab85356f
bruno10-0 <brunobrodon75@gmail.com>
2023-05-05 17:42:09 -0300

    primero commit

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        IguCal calcul = new IguCal();
+        calcul.setVisible(true);
+        calcul.setLocationRelativeTo(null);
+    }
+}
