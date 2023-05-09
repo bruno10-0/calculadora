afb829febd8ec0d8da4ee28b4ad86319d1103ab9
bruno10-0 <brunobrodon75@gmail.com>
2023-05-09 00:08:43 -0300

    funcionalidad

--- src/main/java/com/mycompany/calculadora/main.java
+++ src/main/java/com/mycompany/calculadora/main.java
@@ -8,7 +8,7 @@ package com.mycompany.calculadora;
  *
  * @author Bruno
  */
-public class Calculadora {
+public class main {
 
     public static void main(String[] args) {
         IguCal calcul = new IguCal();
